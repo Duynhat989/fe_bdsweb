659839cc719768f518c07f9799c91deb2db8ac4d
duynhat989 <vietduy989kc@gmail.com>
2024-12-23 10:40:36 +0700

    update icon 1

--- src/views/ContractView.vue
+++ src/views/ContractView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import contract_ct from '@/assets/images/contract_ct.png';
-import create_file from '@/assets/images/create_file.png';
+import contract_scan from '@/assets/images/contract_scan.png';
+import create_file_new from '@/assets/images/create_file_new.png';
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import { onMounted, ref } from 'vue';
 const isLoading = ref(false);
@@ -20,14 +20,14 @@ console.log(isLoading);
     </div>
     <div class="contract-options">
       <div class="contract-box">
-        <img :src="contract_ct" alt="Rà soát hợp đồng" class="contract-image" />
+        <img :src="contract_scan" alt="Rà soát hợp đồng" class="contract-image" />
         <h2>Rà soát hợp đồng</h2>
         <p>Kiểm tra và phân tích hợp đồng của bạn để đảm bảo tuân thủ và tối ưu hóa lợi ích hợp pháp.</p>
         <a href="/contract/review" class="contract-button"><i class='bx bx-memory-card'></i> Rà soát ngay</a>
       </div>
 
       <div class="contract-box">
-        <img :src="create_file" alt="Tạo hợp đồng" class="contract-image" />
+        <img :src="create_file_new" alt="Tạo hợp đồng" class="contract-image" />
         <h2>Tạo hợp đồng</h2>
         <p>Tạo mới hợp đồng theo nhu cầu của bạn với các mẫu có sẵn và tùy chỉnh dễ dàng.</p>
         <a href="/contract/create" class="contract-button"><i class='bx bxs-paint' ></i>  Tạo mới</a>
